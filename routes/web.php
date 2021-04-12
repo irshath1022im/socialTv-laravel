27ab92f2e3fc541312435515ee864055b6e52253
irshath <irshath@irshathinfo.com>
2021-04-12 10:16:44 +0300

    update the web.php

--- routes/web.php
+++ routes/web.php
@@ -32,83 +32,22 @@ Route::resource('/adminSubCategory', AdminSubCategoryController::class);
 Route::resource('/adminPost', AdminPostController::class);
 
 
-Route::get('/{any?}', function () {
-    return view('welcomevue');
-})->where('any', '^(?!api\/)[\/\w\.\,-]*');
+// Route::get('/{any?}', function () {
+//     return view('welcomevue');
+// })->where('any', '^(?!api\/)[\/\w\.\,-]*');
 
 
-// Route::get('/', function () {
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
-//     // $from = date('2021-01-01');
-//     // $to = date('2021-03-02');
+Route::get('/subCategory/{category}', function ($category) {
 
-//     // $result =Category::with(['posts' => function($query) use($from,$to)
-//     // {
-//     //     return $query->whereBetween('created_at', [$from, $to]);
-//     // }])->get();
-//     // $result = Category::with(['subCategory' => function($query)
-//     // {
-//     //     return $query -> withCount('posts')->with('posts');
-//     // }])->get();
-//     //     return $result;
+    $result = Post::where('subCategoryId', $category)
+            ->orderByDesc('created_at')
+            ->paginate(5);
 
-//     return view('welcome');
-// })->name('home');
+    // return $result;
+    return view('subCategory', ['subCategoryPosts' => $result]);
 
-
-
-// // Route::get('/navigation',  function(){
-
-// //     $category = Category::with(['subCategory' => function($query){
-// //         return $query->get()->pluck('subCategory');
-// //     }])->get();
-// //     // return $category;
-// //     return view('components.navbar', ['category'=>$category]);
-// // });
-
-// // Route::get('/recentPosts', function()
-// // {
-// //     $result = Post::orderByDesc('created_at')->take(5)->get();
-// //     return $result;
-// // });
-
-
-
-// Route::get('/subCategory/{category}', function ($category) {
-//     // return $category;
-//     // $result = SubCategory::with(['posts' => function($query){
-//     //         return $query->paginate(5);
-//     // }])
-//     //             ->where('id', $category)
-//     //             ->get()
-//     //             ;
-
-//     $result = Post::where('subCategoryId', $category)
-//             ->orderByDesc('created_at')
-//             ->paginate(5);
-
-//     // return $result;
-//     return view('subCategory', ['subCategoryPosts' => $result]);
-
-// })->name('subCategory');
-
-// // Route::get('test/{id}', function ($id) {
-// //     $result = Category::with(['posts' => function($query){
-// //         return $query->orderByDesc('created_at')->take(3);
-// //     }])
-// //     ->where('id', $id)
-// //     ->get();
-
-
-
-// //     return $result;
-// // });
-
-// Route::get('test', function () {
-
-//     $result =Post::where('postType', 'trending')
-//             ->orderByDesc('created_at')
-//             ->get();
-
-//     return $result;
-// });
+})->name('subCategory');
