4b22fb26d193643d19a17ac7e882e1c142f89596
irshath <irshath@irshathinfo.com>
2021-04-12 10:08:29 +0300

    add google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,8 +37,21 @@
         <link rel="stylesheet" href="{{ asset('css/Bold-BS4-Cards-with-Hover-Effect-74.css')}}">
         <link rel="stylesheet" href="{{ asset('css/header.css')}}">
         <link rel="stylesheet" href="{{ asset('css/Responsive-footer.css')}}">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135001405-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-135001405-1');
+</script>
+
         @livewireStyles
         @livewireScripts
+
+
 
     </head>
     <body class="container">
--- routes/web.php
+++ routes/web.php
@@ -32,90 +32,83 @@ Route::resource('/adminSubCategory', AdminSubCategoryController::class);
 Route::resource('/adminPost', AdminPostController::class);
 
 
-// Route::get('/{any?}', function () {
-//     return view('welcomevue');
-// })->where('any', '^(?!api\/)[\/\w\.\,-]*');
+Route::get('/{any?}', function () {
+    return view('welcomevue');
+})->where('any', '^(?!api\/)[\/\w\.\,-]*');
 
 
-Route::get('/', function () {
+// Route::get('/', function () {
 
-    // $from = date('2021-01-01');
-    // $to = date('2021-03-02');
+//     // $from = date('2021-01-01');
+//     // $to = date('2021-03-02');
 
-    // $result =Category::with(['posts' => function($query) use($from,$to)
-    // {
-    //     return $query->whereBetween('created_at', [$from, $to]);
-    // }])->get();
-    // $result = Category::with(['subCategory' => function($query)
-    // {
-    //     return $query -> withCount('posts')->with('posts');
-    // }])->get();
-    //     return $result;
+//     // $result =Category::with(['posts' => function($query) use($from,$to)
+//     // {
+//     //     return $query->whereBetween('created_at', [$from, $to]);
+//     // }])->get();
+//     // $result = Category::with(['subCategory' => function($query)
+//     // {
+//     //     return $query -> withCount('posts')->with('posts');
+//     // }])->get();
+//     //     return $result;
 
-    return view('welcome');
-})->name('home');
+//     return view('welcome');
+// })->name('home');
 
 
 
-// Route::get('/navigation',  function(){
+// // Route::get('/navigation',  function(){
 
-//     $category = Category::with(['subCategory' => function($query){
-//         return $query->get()->pluck('subCategory');
-//     }])->get();
+// //     $category = Category::with(['subCategory' => function($query){
+// //         return $query->get()->pluck('subCategory');
+// //     }])->get();
+// //     // return $category;
+// //     return view('components.navbar', ['category'=>$category]);
+// // });
+
+// // Route::get('/recentPosts', function()
+// // {
+// //     $result = Post::orderByDesc('created_at')->take(5)->get();
+// //     return $result;
+// // });
+
+
+
+// Route::get('/subCategory/{category}', function ($category) {
 //     // return $category;
-//     return view('components.navbar', ['category'=>$category]);
-// });
+//     // $result = SubCategory::with(['posts' => function($query){
+//     //         return $query->paginate(5);
+//     // }])
+//     //             ->where('id', $category)
+//     //             ->get()
+//     //             ;
 
-// Route::get('/recentPosts', function()
-// {
-//     $result = Post::orderByDesc('created_at')->take(5)->get();
-//     return $result;
-// });
+//     $result = Post::where('subCategoryId', $category)
+//             ->orderByDesc('created_at')
+//             ->paginate(5);
 
-Route::get('/postsByWeek', function () {
+//     // return $result;
+//     return view('subCategory', ['subCategoryPosts' => $result]);
 
+// })->name('subCategory');
 
-    // $result = Post::orderByDesc('created_at')->get();
-    // return $result;
-});
-
-
-
-Route::get('/subCategory/{category}', function ($category) {
-    // return $category;
-    // $result = SubCategory::with(['posts' => function($query){
-    //         return $query->paginate(5);
-    // }])
-    //             ->where('id', $category)
-    //             ->get()
-    //             ;
-
-    $result = Post::where('subCategoryId', $category)
-            ->orderByDesc('created_at')
-            ->paginate(5);
-
-    // return $result;
-    return view('subCategory', ['subCategoryPosts' => $result]);
-
-})->name('subCategory');
-
-// Route::get('test/{id}', function ($id) {
-//     $result = Category::with(['posts' => function($query){
-//         return $query->orderByDesc('created_at')->take(3);
-//     }])
-//     ->where('id', $id)
-//     ->get();
+// // Route::get('test/{id}', function ($id) {
+// //     $result = Category::with(['posts' => function($query){
+// //         return $query->orderByDesc('created_at')->take(3);
+// //     }])
+// //     ->where('id', $id)
+// //     ->get();
 
 
+
+// //     return $result;
+// // });
+
+// Route::get('test', function () {
+
+//     $result =Post::where('postType', 'trending')
+//             ->orderByDesc('created_at')
+//             ->get();
 
 //     return $result;
 // });
-
-Route::get('test', function () {
-
-    $result =Post::where('postType', 'trending')
-            ->orderByDesc('created_at')
-            ->get();
-
-    return $result;
-});
